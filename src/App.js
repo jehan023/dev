1fc4a8c717f66749a901a5bdc04775b34823c68b
Jehan Hadjisaid <57952278+jehan023@users.noreply.github.com>
2023-08-23 23:59:57 +0800

    no navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 import './App.scss';
+import 'animate.css';
+import { LuMail } from "react-icons/lu";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 //components
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Work from './components/Work';
 import Expertise from './components/Expertise';
+import ScrollButton from './components/ScrollButton';
 
 function App() {
   return (
@@ -22,6 +26,14 @@ function App() {
         <Work />
         <Contact />
       </main>
+      <div className="side-links-wrapper animate__animated animate__fadeInUp animate__delay-1s">
+        <a href="mailto:jlhadjisaid@gmail.com"><LuMail className='link-icon' /></a>
+        <a href="https://github.com/jehan023" target="_blank" rel="noreferrer"><BsGithub className='link-icon' /></a>
+        <a href="https://www.linkedin.com/in/jehanhadjisaid/" target="_blank" rel="noreferrer"><BsLinkedin className='link-icon' /></a>
+        <div className="links-block"></div>
+      </div>
+
+      <ScrollButton />
     </div>
   );
 }
--- src/components/Expertise.js
+++ src/components/Expertise.js
@@ -1,50 +1,84 @@
+import { useEffect } from 'react';
 import { HiOutlineComputerDesktop } from 'react-icons/hi2';
 import { FaReact } from "react-icons/fa";
 import { IoLogoNodejs } from "react-icons/io";
 import HelloWord from '../images/hello-world-html-code.png';
+// import { useInView } from "react-intersection-observer"
 
 export default function Expertise() {
+  // const containerRef = useRef();
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }
+      });
+    },{
+      threshold: 0.5
+    });
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+  }, [])
+
+  // const [ref, inView] = useInView({ threshold: .5 })
+  // const [animationRun, setAnimationRun] = useState(false);
+
+  // const handleAnimationEnd = () => {
+  //   setAnimationRun(true);
+  // };
+
   return (
-    <div className="expertise-container" id="expertise">
-      <h2 className="container-heading-title">My Expertise</h2>
+    <div className="expertise-container" id="expertise" >
+      <div>
+        {/* <h2 className={`${inView && !animationRun ? "container-heading-title animate__animated animate__fadeInUp" : "container-heading-title"}`}
+          onAnimationEnd={handleAnimationEnd}>My Expertise</h2> */}
+        <h2 className="container-heading-title hidden">My Expertise</h2>
 
-      <section className="expertise-section">
-        <div className="expertise-section-box">
-          <div className="section-header">
-            <HiOutlineComputerDesktop className='icon-box-icon' />
-            <h5 className='icon-box-title'><span class="chonky-underline-megenta">Software</span><br />Development</h5>
-          </div>
-          <div className='section-box-content'>
-            <div className='section-box-desc'>
-              Experienced in both functional and OOP: C#, Python, Java, JavaScript, TypeScript.
+        <section className="expertise-section">
+          {/* <div className={`${inView && !animationRun ? "expertise-section-box animate__animated animate__fadeInUp" : "expertise-section-box"}`}
+            onAnimationEnd={handleAnimationEnd}> */}
+          <div className="expertise-section-box hidden">
+            <div className="section-header">
+              <HiOutlineComputerDesktop className='icon-box-icon' />
+              <h5 className='icon-box-title'><span className="chonky-underline-megenta">Software</span><br />Development</h5>
+            </div>
+            <div className='section-box-content'>
+              <div className='section-box-desc'>
+                Experienced in both functional and OOP: C#, Python, Java, JavaScript, TypeScript.
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="expertise-section-box">
-          <div className="section-header">
-            <FaReact className='icon-box-icon' />
-            <h5 className='icon-box-title'><span class="chonky-underline-blue">Frontend Dev</span><br />React, NextJS</h5>
-          </div>
-          <div className='section-box-content'>
-            <div className='section-box-desc'>
-              Passionate about UI/UX. With years of development experience in HTML, CSS, JS, React and NextJS frameworks.
+          {/* <div className={`${inView && !animationRun ? "expertise-section-box animate__animated animate__fadeInUp" : "expertise-section-box"}`}
+            onAnimationEnd={handleAnimationEnd}> */}
+          <div className="expertise-section-box hidden">
+            <div className="section-header">
+              <FaReact className='icon-box-icon' />
+              <h5 className='icon-box-title'><span className="chonky-underline-blue">Frontend Dev</span><br />React, NextJS</h5>
+            </div>
+            <div className='section-box-content'>
+              <div className='section-box-desc'>
+                Passionate about UI/UX. With years of development experience in HTML, CSS, JS, React and NextJS frameworks.
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="expertise-section-box">
-          <div className="section-header">
-            <IoLogoNodejs className='icon-box-icon' />
-            <h5 className='icon-box-title'><span class="chonky-underline-yellow">Backend Dev</span><br />Web, Mobile</h5>
-          </div>
-          <div className='section-box-content'>
-            <div className='section-box-desc'>
-              Experienced in PHP, NodeJS, and databases, delivering effective web and mobile solutions.
+          {/* <div className={`${inView && !animationRun ? "expertise-section-box animate__animated animate__fadeInUp" : "expertise-section-box"}`}
+            onAnimationEnd={handleAnimationEnd}> */}
+          <div className="expertise-section-box hidden">
+            <div className="section-header">
+              <IoLogoNodejs className='icon-box-icon' />
+              <h5 className='icon-box-title'><span className="chonky-underline-yellow">Backend Dev</span><br />Web, Mobile</h5>
+            </div>
+            <div className='section-box-content'>
+              <div className='section-box-desc'>
+                Experienced in PHP, NodeJS, and databases, delivering effective web and mobile solutions.
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
 
       <div className='background-image-overlay'>
         <img src={HelloWord} alt='helloword' className='image-code' />
