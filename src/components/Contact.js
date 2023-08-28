1a0c3ff9d741ee82a0fba1ff3a71c5708109bfd6
Jehan Hadjisaid <jehanhadjisaid.023@gmail.com>
2023-08-28 15:54:24 +0800

    edit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <div className="contact-container" id="contact">
-      <div className='background-overlay'>
+      <div className="background-overlay">
         <div className="container-heading-title">
           <h4>What's Next?</h4>
         </div>
@@ -9,16 +9,20 @@ export default function Contact() {
           <h3>Get In Touch</h3>
         </div>
         <div className="section-subheading">
-          Have a question or want to work together? My inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+          Have a question or want to work together? My inbox is always open.
+          Whether you have a question or just want to say hi, I’ll try my best
+          to get back to you!
         </div>
         <div className="section-contact-btn">
-          <a class="custom-btn-1" href="mailto:jlhadjisaid@gmail.com"><span>Say Hello!</span></a>
+          <a class="custom-btn-1" href="mailto:jlhadjisaid@gmail.com">
+            <span>Say Hello!</span>
+          </a>
         </div>
 
         <div className="footer-section">
           © 2023 Jehan Hadjisaid. Made with 💖
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
